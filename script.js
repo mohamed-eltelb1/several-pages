81f6824538de911a9621ff18bc4dd2a431da15dc
Mohamed Eltelb <160356087+mohamed-eltelb1@users.noreply.github.com>
2024-04-27 17:17:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// var image1 = document.getElementById('image1');
+
+// var right = document.getElementById('right');
+
+// var left = document.getElementById('left');
+
+// right.addEventListener('click' , function(){
+//     image1.scrollLeft += 1400;
+// })
+
+// left.addEventListener('click' , function(){
+//     image1.scrollLeft -= 1400;
+// })
